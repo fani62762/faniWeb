0d0bff676e7bfdaa66bbf736f2ee8bd2153e9ccf
fani62762 <fani62762@gmail.com>
2023-05-30 13:37:55 +0300

    yr

--- lib/dashboard/Admin/Table/typeT.dart
+++ lib/dashboard/Admin/Table/typeT.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:faniweb/main.dart';
 
-class Service {
+class Type {
+  String id;
   String name;
-  String type;
 
-  Service({required this.name, required this.type});
+  Type({required this.id, required this.name});
 }
 
-class Type {
+class Service {
+  String id;
   String name;
+  String typeId;
 
-  Type({required this.name});
+  Service({required this.id, required this.name, required this.typeId});
 }
 // Rest of the code...
 
@@ -33,7 +35,7 @@ String? chosenColumnName = columnNames.first;
 
 class _ServiceManagementScreenState extends State<ServiceManagementScreen> {
   List<Type> types = [];
-  List<Map<Type, List<Service>>> typeServices = [];
+  List<Service> services = []; // List of services
 
   TextEditingController typeNameController = TextEditingController();
   TextEditingController serviceNameController = TextEditingController();
@@ -44,23 +46,12 @@ class _ServiceManagementScreenState extends State<ServiceManagementScreen> {
     servs = allserv;
     // Initial data
     types = [
-      Type(name: 'Type 1'),
-      Type(name: 'Type 2'),
+      Type(id: '1', name: 'Type 1'),
+      Type(id: '2', name: 'Type 2'),
     ];
-    typeServices = [
-      {
-        types[0]: [
-          Service(name: 'Service 1', type: 'Type 1'),
-          Service(name: 'Service 2', type: 'Type 1'),
-        ]
-      },
-      {
-        types[1]: [
-          Service(name: 'Service 3', type: 'Type 2'),
-          Service(name: 'Service 4', type: 'Type 2'),
-          Service(name: 'Service 5', type: 'Type 2'),
-        ]
-      },
+    services = [
+      Service(id: '1', name: 'Service 1', typeId: '1'),
+      Service(id: '2', name: 'Service 2', typeId: '2'),
     ];
   }
 
@@ -71,11 +62,11 @@ class _ServiceManagementScreenState extends State<ServiceManagementScreen> {
     super.dispose();
   }
 
-  void addType(String typeName) {
-    setState(() {
-      types.add(Type(name: typeName));
-    });
-  }
+  // void addType(String typeName) {
+  //   setState(() {
+  //     types.add(Type(name: typeName));
+  //   });
+  // }
 
   void deleteType(Type type) {
     setState(() {
@@ -83,24 +74,24 @@ class _ServiceManagementScreenState extends State<ServiceManagementScreen> {
     });
   }
 
-  void addService(String serviceName, Type type) {
-    final index = types.indexOf(type);
-    if (index != -1) {
-      setState(() {
-        typeServices[index][type]!
-            .add(Service(name: serviceName, type: type.name));
-      });
-    }
-  }
+  // void addService(String serviceName, Type type) {
+  //   final index = types.indexOf(type);
+  //   if (index != -1) {
+  //     setState(() {
+  //       typeServices[index][type]!
+  //           .add(Service(name: serviceName, type: type.name));
+  //     });
+  //   }
+  // }
 
-  void deleteService(Service service, Type type) {
-    final index = types.indexOf(type);
-    if (index != -1) {
-      setState(() {
-        typeServices[index][type]!.remove(service);
-      });
-    }
-  }
+  // void deleteService(Service service, Type type) {
+  //   final index = types.indexOf(type);
+  //   if (index != -1) {
+  //     setState(() {
+  //       typeServices[index][type]!.remove(service);
+  //     });
+  //   }
+  // }
 
   void searchOrders(String? columnName, String query) {
     String columnNameeng = "";
@@ -124,369 +115,66 @@ class _ServiceManagementScreenState extends State<ServiceManagementScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Types and Services'),
       ),
-      padding: EdgeInsets.all(20),
-      child: StatefulBuilder(
-        builder: (context, setState) {
-          return Column(
-            children: [
-              DropdownButtonFormField<String>(
-                value: chosenColumnName,
-                decoration: InputDecoration(
-                  labelText: '  بحث على حسب',
+      body: ListView.builder(
+        itemCount: types.length,
+        itemBuilder: (context, index) {
+          Type type = types[index];
+          List<Service> typeServices =
+              services.where((service) => service.typeId == type.id).toList();
+
+          return Card(
+            child: ExpansionTile(
+              title: Text(type.name),
+              children: [
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: typeServices.length,
+                  itemBuilder: (context, index) {
+                    Service service = typeServices[index];
+                    return ListTile(
+                      title: Text(service.name),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          setState(() {
+                            // Delete the service
+                            // services.remove(service);
+                          });
+                        },
+                      ),
+                    );
+                  },
                 ),
-                onChanged: (String? value) {
-                  setState(() {
-                    chosenColumnName = value ?? '';
-                  });
-                },
-                items: columnNames.map((String columnName) {
-                  return DropdownMenuItem<String>(
-                    value: columnName,
-                    child: Center(child: Text(columnName)),
-                  );
-                }).toList(),
-              ),
-              TextField(
-                controller: _searchController,
-                decoration: InputDecoration(
-                  labelText: 'بحث',
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.clear),
+                ListTile(
+                  title: Text('Add Service'),
+                  trailing: IconButton(
+                    icon: Icon(Icons.add),
                     onPressed: () {
-                      setState(() {
-                        _searchController.clear();
-                      });
+                      // Open a dialog to add a new service
+                      // showDialog(
+                      //   context: context,
+                      //   builder: (context) => AddServiceDialog(
+                      //     onServiceAdded: (newService) {
+                      //       setState(() {
+                      //         // Add the new service
+                      //         services.add(newService);
+                      //       });
+                      //     },
+                      //   ),
+                      // );
                     },
                   ),
                 ),
-                onChanged: (value) {
-                  setState(() {
-                    searchOrders(chosenColumnName, value);
-                  });
-                },
-              ),
-              SizedBox(height: 20),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: DataTable(
-                  columnSpacing: 12,
-                  dataRowHeight: 60,
-                  dividerThickness: 1,
-                  columns: [
-                    DataColumn(
-                      label: Text('اسم الخدمة'),
-                    ),
-                    DataColumn(label: _verticalDivider),
-                    for (Type type in types)
-                      DataColumn(
-                        label: Text(type.name),
-                      ),
-                  ],
-                  rows: servs.map((serv) {
-                    List<DataCell> cells = [
-                      DataCell(Text(serv['name'])),
-                      DataCell(_verticalDivider),
-                    ];
-
-                    for (Type type in types) {
-                      List<Service> services =
-                          typeServices[types.indexOf(type)][type] ?? [];
-
-                      cells.add(
-                        DataCell(
-                          Container(
-                            child: Checkbox(
-                              value: services.contains(
-                                Service(
-                                  name: serv['name'],
-                                  type: type.name,
-                                ),
-                              ),
-                              onChanged: (value) {
-                                setState(() {
-                                  if (value == true) {
-                                    addService(serv['name'], type);
-                                  } else {
-                                    deleteService(
-                                      Service(
-                                        name: serv['name'],
-                                        type: type.name,
-                                      ),
-                                      type,
-                                    );
-                                  }
-                                });
-                              },
-                            ),
-                          ),
-                        ),
-                      );
-                    }
-
-                    return DataRow(cells: cells);
-                  }).toList(),
-                ),
-              ),
-            ],
+              ],
+            ),
           );
         },
       ),
     );
   }
-
-  Widget _verticalDivider = const VerticalDivider(
-    color: Colors.grey,
-    thickness: .5,
-  );
 }
-
-
-// class ServiceManagementScreen extends StatefulWidget {
-//   @override
-//   _ServiceManagementScreenState createState() =>
-//       _ServiceManagementScreenState();
-// }
-
-// List<Map<String, dynamic>> servs = [];
-// TextEditingController _searchController = TextEditingController();
-// List<String> columnNames = [
-//   'الخدمة ',
-//   'نوع الخدمة',
-
-//   // Add other column names as needed
-// ];
-// String? chosenColumnName = columnNames.first;
-
-// class _ServiceManagementScreenState extends State<ServiceManagementScreen> {
-//   List<Type> types = [];
-//   List<Map<Type, List<Service>>> typeServices = [];
-
-//   TextEditingController typeNameController = TextEditingController();
-//   TextEditingController serviceNameController = TextEditingController();
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     servs = allserv;
-//     // Initial data
-//     types = [
-//       Type(name: 'Type 1'),
-//       Type(name: 'Type 2'),
-//     ];
-//     typeServices = [
-//       {
-//         types[0]: [
-//           Service(name: 'Service 1', type: 'Type 1'),
-//           Service(name: 'Service 2', type: 'Type 1'),
-//         ]
-//       },
-//       {
-//         types[1]: [
-//           Service(name: 'Service 3', type: 'Type 2'),
-//           Service(name: 'Service 4', type: 'Type 2'),
-//           Service(name: 'Service 5', type: 'Type 2'),
-//         ]
-//       },
-//     ];
-//   }
-
-//   @override
-//   void dispose() {
-//     typeNameController.dispose();
-//     serviceNameController.dispose();
-//     super.dispose();
-//   }
-
-//   void addType(String typeName) {
-//     setState(() {
-//       types.add(Type(name: typeName));
-//     });
-//   }
-
-//   void deleteType(Type type) {
-//     setState(() {
-//       types.remove(type);
-//     });
-//   }
-
-//   void addService(String serviceName, Type type) {
-//     final index = types.indexOf(type);
-//     if (index != -1) {
-//       setState(() {
-//         typeServices[index][type]!
-//             .add(Service(name: serviceName, type: type.name));
-//       });
-//     }
-//   }
-
-//   void deleteService(Service service, Type type) {
-//     final index = types.indexOf(type);
-//     if (index != -1) {
-//       setState(() {
-//         typeServices[index][type]!.remove(service);
-//       });
-//     }
-//   }
-
-//   void searchOrders(String? columnName, String query) {
-//     String columnNameeng = "";
-//     int stat;
-//     if (columnName == columnNames[0]) {
-//       columnNameeng = "name";
-//     }
-//     if (columnName == columnNames[1]) {
-//       columnNameeng = "type";
-//     }
-
-//     setState(() {
-//       servs = allserv.where((serv) {
-//         // Replace 'columnName' with the actual property name of the chosen column
-//         // in the 'order' object.
-//         final value = serv[columnNameeng].toString();
-//         return value.toLowerCase().contains(query.toLowerCase());
-//       }).toList();
-//     });
-//   }
-
-//   // Rest of the code...
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       decoration: BoxDecoration(
-//         color: Colors.white,
-//         borderRadius: BorderRadius.circular(20),
-//       ),
-//       padding: EdgeInsets.all(20),
-//       child: StatefulBuilder(
-//         builder: (context, setState) {
-//           return Column(
-//             children: [
-//               DropdownButtonFormField<String>(
-//                 value: chosenColumnName,
-//                 decoration: InputDecoration(
-//                   labelText: '  بحث على حسب',
-//                 ),
-//                 onChanged: (String? value) {
-//                   setState(() {
-//                     chosenColumnName = value ?? '';
-//                   });
-//                 },
-//                 items: columnNames.map((String columnName) {
-//                   return DropdownMenuItem<String>(
-//                     value: columnName,
-//                     child: Center(child: Text(columnName)),
-//                   );
-//                 }).toList(),
-//               ),
-//               TextField(
-//                 controller: _searchController,
-//                 decoration: InputDecoration(
-//                   labelText: 'بحث',
-//                   suffixIcon: IconButton(
-//                     icon: Icon(Icons.clear),
-//                     onPressed: () {
-//                       setState(() {
-//                         _searchController.clear();
-//                       });
-//                     },
-//                   ),
-//                 ),
-//                 onChanged: (value) {
-//                   setState(() {
-//                     // Call the search function with the chosen column name and search query
-//                     searchOrders(chosenColumnName, value);
-//                   });
-//                 },
-//               ),
-//               SizedBox(height: 20),
-//               SingleChildScrollView(
-//                 scrollDirection: Axis.horizontal,
-//                 child: DataTable(
-//                   columnSpacing: 12,
-//                   dataRowHeight: 60,
-//                   dividerThickness: 1,
-//                   columns: [
-//                     DataColumn(
-//                       label: Text('اسم الخدمة'),
-//                     ),
-//                     DataColumn(label: _verticalDivider),
-//                     // DataColumn(
-//                     //   label: Text('اسم العامل'),
-//                     //   numeric: true,
-//                     // ),
-//                     // DataColumn(label: _verticalDivider),
-//                     // DataColumn(
-//                     //   label: Text('حالة الطلب'),
-//                     //   numeric: true,
-//                     // ),
-//                     // DataColumn(label: _verticalDivider),
-//                     // DataColumn(
-//                     //   label: Text('تفاصيل'),
-//                     // ),
-//                     // DataColumn(label: _verticalDivider),
-//                   ],
-//                   rows: servs.map((serv) {
-//                     List<DataCell> cells = [
-//                       DataCell(Text(serv['name'])),
-//                       DataCell(_verticalDivider),
-//                       // DataCell(Text(serv['Wname'])),
-//                       // DataCell(_verticalDivider),
-//                     ];
-
-//                     // List<Service> services =
-//                     //     typeServices[serv['type']][serv['type']] ?? [];
-
-//                     // for (Service service in services) {
-//                     //   cells.add(
-//                     //     DataCell(
-//                     //       Container(
-//                     //         child: Checkbox(
-//                     //           value: serv['services'].contains(service),
-//                     //           onChanged: (value) {
-//                     //             setState(() {
-//                     //               if (value == true) {
-//                     //                 serv['services'].add(service);
-//                     //               } else {
-//                     //                 serv['services'].remove(service);
-//                     //               }
-//                     //             });
-//                     //           },
-//                     //         ),
-//                     //       ),
-//                     //     ),
-//                     //   );
-//                     //   cells.add(DataCell(_verticalDivider));
-//                     // }
-
-//                     // cells.addAll([
-//                     //   DataCell(IconButton(
-//                     //     icon: Icon(Icons.info_outline),
-//                     //     onPressed: () {
-//                     //       showOrderDetails(serv);
-//                     //     },
-//                     //   )),
-//                     //   DataCell(_verticalDivider),
-//                     // ]);
-
-//                     return DataRow(cells: cells);
-//                   }).toList(),
-//                 ),
-//               ),
-//             ],
-//           );
-//         },
-//       ),
-//     );
-//   }
-
-//   Widget _verticalDivider = const VerticalDivider(
-//     color: Colors.grey,
-//     thickness: .5,
-//   );
-// }
